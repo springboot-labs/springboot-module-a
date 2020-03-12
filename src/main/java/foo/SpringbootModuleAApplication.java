f5ff0da76f5a953df5e6069c5d82bcdcb3691418
jcruz <jcruz@tre-pa.jus.br>
2020-03-12 17:58:41 -0300

    First Commit

---
+++ src/main/java/foo/SpringbootModuleAApplication.java
@@ -0,0 +1,13 @@
+package foo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootModuleAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootModuleAApplication.class, args);
+    }
+
+}
---
+++ src/test/java/foo/SpringbootModuleAApplicationTests.java
@@ -0,0 +1,13 @@
+package foo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootModuleAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
