78244ecda7cd9cd8016cfcdb43deddf37fc910ec
jcruz <jcruz@tre-pa.jus.br>
2020-03-12 21:41:15 -0300

    Ajustes

--- src/main/java/foo/ModuleATask.java
+++ src/main/java/foo/ModuleATask.java
@@ -1,12 +1,23 @@
 package foo;
 
 import foo.api.Task;
+import foo.service.GenericService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("foo.ModuleATask")
 public class ModuleATask implements Task {
+
+    @Autowired
+    private GenericService genericService;
+
+    private String name = "Lorem Ipsum is simply dummy text of the printing and typesetting industry";
+
     @Override
     public void run() {
         System.out.println("Em ModuleA Task!!!!!!!!!!!");
+        System.out.println(StringUtils.abbreviate(name, 20));
+        genericService.exec();
     }
 }
---
+++ src/main/java/foo/service/GenericService.java
@@ -0,0 +1,11 @@
+package foo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GenericService {
+
+    public void exec() {
+        System.out.println("Em Generic Service!!!!!!!!!!!");
+    }
+}
